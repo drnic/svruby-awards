974e6fb4140ef6b0dac26ec11c8d36ab24c852fe
Dr Nic Williams <drnicwilliams@gmail.com>
2011-04-20 14:37:44 -0500

    fixed Syck's error about yaml reference in env.yml

--- lib/env_yaml.rb
+++ lib/env_yaml.rb
@@ -1,8 +1,8 @@
 require 'yaml'
 begin
-  env_yaml = YAML.load_file(File.dirname(__FILE__) + '/../config/env.yml')
+  env_yaml = YAML.load_file(File.expand_path('../../config/env.yml', __FILE__))
   if env_hash = env_yaml[ENV['RACK_ENV'] || ENV['RAILS_ENV'] || 'development']
-    puts env_hash.inspect
+    # puts env_hash.inspect
     env_hash.each_pair do |k,v|
       ENV[k] = v.to_s
     end
