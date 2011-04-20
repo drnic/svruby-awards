7f98b302a9e4db03bcf22a13dae4a885c7dbc8c6
Dr Nic Williams <drnicwilliams@gmail.com>
2011-04-20 12:57:24 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SvrubyAwards::Application.config.secret_token = '0edb4ad4014e320da0d7703f187f9116addc9bb7d49886abb9057c6d61c73e6e8a05b3bdae5469f5ab8db2a1aabbb6e868b02f135ba4587884afe696f558d3d1'
